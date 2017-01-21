dbe2f8919c6759a568a791d69ddefc226a81aac3
Elionardo Feliciano <efelici1@gmail.com>
2017-01-21 14:08:56 -0600

    Add some art and All Scripts so far (Game, CharacterController)

---
+++ GGJ2017/GGJ2017/Assets/z_Elio/Scripts/CharacterController.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+
+//Input Control
+//Collision control
+public class CharacterController : MonoBehaviour
+{
+    private GameObject particle;
+
+    private KeyCode jump = KeyCode.Alpha0;
+    private int instance;
+
+    private float waveTime;
+    private float waveAmp = .25f;
+    private float waveFrequency = 1f;
+
+    private static Vector2 CHARACTER_ORIGIN_POSITION = new Vector2(-20, 0);
+
+    public void CreateCharacterController(KeyCode jumpIn, int instanceIn)
+    {
+        particle = Instantiate(Resources.Load("Particle")) as GameObject;
+        particle.gameObject.transform.parent = gameObject.transform;
+
+        //CreateParticles();
+
+        jump = jumpIn;
+        instance = instanceIn;
+    }
+
+    public void PositionCharacter(int totalCharacters)
+    {
+        if(totalCharacters == 1) //Hack
+        {
+            this.gameObject.transform.position = CHARACTER_ORIGIN_POSITION;
+        }
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+    void FixedUpdate()
+    {
+
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        ParticleMotion();
+
+        if (jump != KeyCode.Alpha0 && Input.GetKeyDown(jump))
+        {
+            Debug.Log("CharacterController: Update: "+ instance +": jump");
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+
+    }
+
+    private void ParticleMotion()
+    {
+        waveTime += Time.deltaTime;
+        particle.transform.position = new Vector3(particle.transform.position.x, particle.transform.position.y + waveAmp * Mathf.Sin((waveTime % 1) * (waveFrequency * 2) * Mathf.PI), particle.transform.position.z);
+    }
+
+    /*
+    public void CreateParticles()
+    {
+        int repeat = 1;
+        int frequency = 2;//repeatrate
+        float amp = .25f;//repeatrate
+        float resolution = 10;//the pointamount used to create the wave
+
+        ParticleSystem.VelocityOverLifetimeModule vel = particle.GetComponent<ParticleSystem>().velocityOverLifetime;
+        vel.enabled = true;
+        vel.space = ParticleSystemSimulationSpace.Local;
+
+        AnimationCurve curveX = new AnimationCurve();
+
+        Vector3 WaveValue = new Vector3(0,0,0);
+        int points = 100 * repeat;
+        for (int i = 0; i < points; i++)
+        {
+            float newtime = (i / (resolution - 1));
+            WaveValue.x = (amp * Mathf.Sin(newtime * (frequency * 2) * Mathf.PI));
+            WaveValue = WaveValue.normalized * amp;
+
+            curveX.AddKey(newtime, WaveValue.x);
+        }
+
+        vel.x = new ParticleSystem.MinMaxCurve(10.0f, curveX);
+    }//*/
+}
---
+++ GGJ2017/GGJ2017/Assets/z_Elio/Scripts/Game.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public static KeyCode COLOR_1 = KeyCode.Keypad8;
+    public static KeyCode COLOR_2 = KeyCode.Keypad6;
+    public static KeyCode COLOR_3 = KeyCode.Keypad2;
+
+    public static int SINGLE_PLAYER_GAME = 0;
+    public static int MULTI_PLAYER_GAME = 1;
+
+    public static int SP_STARTING_WAVES = 1;
+    public static int MP_STARTING_WAVES = 3;
+
+    public static Game game;
+
+    public List<GameObject> Characters;
+    public int iPlayerMode = SINGLE_PLAYER_GAME;
+
+    void Awake()
+    {
+        Debug.Log("Game: Awake:");
+        if(game == null)
+        {
+            Debug.Log("Game: Awake: game is null");
+            DontDestroyOnLoad(this.gameObject);
+            game = this;
+        }
+        else if(game != null)
+        {
+            Debug.Log("Game: Awake: game is not null destroy this instance");
+            Destroy(this.gameObject);
+        }
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        Debug.Log("Game: Start:");
+
+        int charactersToCreate = (iPlayerMode < MULTI_PLAYER_GAME) ? SP_STARTING_WAVES : MP_STARTING_WAVES;
+        Debug.Log("Game: Start: charactersToCreate: " + charactersToCreate);
+
+        Characters = new List<GameObject>();
+        GameObject goCharacter;
+        CharacterController cCharacterController;
+        
+        for(int i = 0; i < charactersToCreate; ++i)
+        {
+            Debug.Log("Game: Start: Loop: " + i);
+            goCharacter = Instantiate(Resources.Load("Player")) as GameObject;
+            cCharacterController = goCharacter.AddComponent<CharacterController>();
+            cCharacterController.CreateCharacterController((KeyCode)GetType().GetField("COLOR_" + (i + 1)).GetValue(this), i);
+
+            cCharacterController.PositionCharacter(charactersToCreate);
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
